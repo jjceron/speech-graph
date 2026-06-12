0e1637d245c68ffc38a8cff5c76a5e5f396baf21
Javier Cerón <javier.ceron@utp.edu.co>
2026-06-12 16:45:56 -0500

    Change TPE sampler to RandomSampler for balanced model comparison

--- src/analysis/regression_optuna.py
+++ src/analysis/regression_optuna.py
@@ -1016,7 +1016,7 @@ def run_one_target(
 
     db_path = output_dir / f"optuna_trials_{experiment_name}.db"
     study = optuna.create_study(
-        sampler=optuna.samplers.TPESampler(seed=seed),
+        sampler=optuna.samplers.RandomSampler(seed=seed),
         pruner=optuna.pruners.MedianPruner(
             n_startup_trials=pruner_startup_trials,
             n_warmup_steps=pruner_warmup_steps,
